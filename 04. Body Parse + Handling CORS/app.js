7d8e655f3e120b750e29ea8001aa280f3d37afca
Mateus Loubach <53960269+mateusceasar@users.noreply.github.com>
2019-09-08 16:08:36 -0300

    Create app.js

---
+++ 04. Body Parse + Handling CORS/app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+
+//To remove ORDERS from RESTAPI, simply delete const(ORDerS) and app.use(ORDERS)
+const contactRoutes = require('./api/routes/contacts');
+const orderRoutes = require('./api/routes/orders');
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((req,res,next) =>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+    "Acess-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access=Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});    
+    }
+    next();
+});
+
+//Routes handling requests
+app.use('/contacts', contactRoutes);
+app.use('/orders', orderRoutes);
+
+app.use ((req, res, next) => {
+    const error = new Error('not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) =>{
+    res.status(error.status ||500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
+module.exports = app
